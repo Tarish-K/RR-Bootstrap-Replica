c0b35e5c86625acc36e4f985681dfd238f71e329
Tarish K <102713730+Tarish-K@users.noreply.github.com>
2022-09-27 23:47:53 -0400

    saved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import TopBar from './componets/TopBar'
+import Background from './componets/Background'
+import StoreListings from './componets/StoreListings'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
+      <StoreListings />
     </div>
   );
 }
